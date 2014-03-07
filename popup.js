1aa654a22313ec1890976803de8d58f71d72de53
coybit <coybit@gmail.com>
2014-03-07 11:39:18 +0330

    clearing

--- popup.js
+++ popup.js
@@ -1,14 +1,12 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// 
 
 var linkDiaryKey = 'linkDiaryQueue';
 
-var linkDiary = {
+var LinkDiary = function() {
 
-  storageKey: 'linkdiary',
+  var storageKey = 'linkdiary';
 
-  putLinkInQueue: function() {
+  this.putLinkInQueue = function() {
 
     chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
 
@@ -20,52 +18,26 @@ var linkDiary = {
         url: activeTab.url,
         favIcon: activeTab.favIcon
       };
-
-      chrome.storage.sync.get('linkDiaryQueue', function(results) {
-
-        var queue = results.linkDiaryQueue || [];
-
-        for( var i=0; i<queue.length; i++ ) {
-
-          if( queue[i].url == tab.url )
-            return;
-
-          console.log(queue[i].title);
-        }
-
-        queue.push(tab);
-
-        chrome.browserAction.setBadgeText({'text':queue.length.toString()});
-
-        chrome.storage.sync.set({'linkDiaryQueue':queue});
-      });
      
+     saveWebPage(tab);
 
     });
-  },
+  }
 
-  initBadgetText: function() {
+  this.initBadgetText = function() {
     chrome.storage.sync.get('linkDiaryQueue', function(results) {
       var queue = results.linkDiaryQueue || [];
       chrome.browserAction.setBadgeText({'text':queue.length.toString()});
     });
   }
-};
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {});
+  this.clear = function() {
+    // Clear queue
+    chrome.browserAction.setBadgeText({'text':'0'});
+    chrome.storage.sync.remove('linkDiaryQueue');
+  }
 
-linkDiary.initBadgetText();
-
-chrome.browserAction.onClicked.addListener(function() {
-  linkDiary.putLinkInQueue();
-});
-
-chrome.contextMenus.create({
-  "title": "Email Today LinksDiary",
-  "contexts": ["all"],
-  "onclick": function(info, tab) {
-
+  this.shareIt = function() {
     chrome.storage.sync.get('linkDiaryQueue', function(results) {
 
       var queue = results.linkDiaryQueue || [];
@@ -77,10 +49,10 @@ chrome.contextMenus.create({
         var title = queue[i].title;
         var date = queue[i].date;
         var url = queue[i].url;
-		
-		var head = encodeURIComponent((i+1) + ') ' + title + ' (' + date + ' )');
-		var description = encodeURIComponent(url);
-		
+    
+    var head = encodeURIComponent((i+1) + ') ' + title + ' (' + date + ' )');
+    var description = encodeURIComponent(url);
+    
         body += head + nl + description + nl + nl
       }
 
@@ -93,6 +65,51 @@ chrome.contextMenus.create({
 
     });
   }
+
+  function saveWebPage(webpage) {
+
+      chrome.storage.sync.get('linkDiaryQueue', function(results) {
+
+        var queue = results.linkDiaryQueue || [];
+
+        // Check wheter it is new or not
+        for( var i=0; i<queue.length; i++ ) {
+
+          if( queue[i].url == webpage.url )
+            return;
+
+          console.log(queue[i].title);
+        }
+
+        queue.push(webpage);
+
+        // Increase badge number
+        chrome.browserAction.setBadgeText({'text':queue.length.toString()});
+
+        // Save web page in queue
+        chrome.storage.sync.set({'linkDiaryQueue':queue});
+      });
+  }
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {});
+
+var linkDiary = new LinkDiary();
+linkDiary.initBadgetText();
+
+chrome.browserAction.onClicked.addListener(function() {
+  linkDiary.putLinkInQueue();
+});
+
+chrome.contextMenus.create({
+  "title": "Email Today LinksDiary",
+  "contexts": ["all"],
+  "onclick": function(info, tab) {
+
+    linkDiary.shareIt();
+
+  }
 });
 
 
@@ -100,9 +117,8 @@ chrome.contextMenus.create({
   "title": "Reset LinksDiary",
   "contexts": ["all"],
   "onclick": function(info, tab) {
+  
+  linkDiary.clear();
 
-    // Clear queue
-    chrome.browserAction.setBadgeText({'text':'0'});
-    chrome.storage.sync.remove('linkDiaryQueue');
   }
 });
