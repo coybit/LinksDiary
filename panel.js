546ef9959443037239c00249baf0d8592ba06332
coybit <coybit@gmail.com>
2014-03-27 17:49:17 +0430

    UI improvment

--- panel.js
+++ panel.js
@@ -43,33 +43,40 @@ function setListBody( queue, isLoggedin ) {
 
 
         var favicon = $('<img>').addClass('itemFavIcon').attr('src',queue[i].favIcon);
-        var seperator = $('<span>|</span>').css('margin','0 1em');
+        var separator = $('<span>|</span>').css('margin','0 1em');
 		var dateLabel = $('<div>').addClass('itemDate').text(queue[i].date);
         var host = $('<div>').addClass('itemHost').text( parseUri(queue[i].url).host );
-        var category = queue[i].category ? $('<div>').addClass('category').text( queue[i].category ) : '';
-
-        var clipboard = $('<a>').addClass('itemCopy').text('Copy');
-
+        var clipboard = $('<a>').addClass('itemCopy')
+            .text('Copy')
+            .attr('title',queue[i].title)
+            .attr('url',queue[i].url);
+        var tweet = $('<a>').addClass('itemTweet')
+            .text('Tweet')
+            .attr('href','https://twitter.com/share?url='+ encodeURI(queue[i].url))
+            .attr('target','_blank');
         var name = $('<a>')
             .addClass('itemTitle')
             .attr('href',queue[i].url)
             .text(queue[i].title)
 
-        var group = $('<span>').addClass('groupName').text( queue[i].group );
+        var group = $('<span>').addClass('groupName').text( queue[i].group + '/' + queue[i].category  );
 
         var title = $('<div>')
             .append(favicon)
             .append(name)
-            .append(group)
-            .append(category)
+            .append(host)
+
+        var subtitle = $('<div>')
+            .append( group )
+            .append( separator.clone() )
+            .append( dateLabel )
+            .append( separator.clone() )
+            .append( clipboard )
+            .append( tweet );
 
 		var header = $('<div>').addClass('itemHeader')
             .append( title )
-            .append( dateLabel )
-            .append( seperator )
-            .append( host )
-            .append( seperator )
-            .append( clipboard );
+            .append( subtitle);
 
 		var body = $('<div>').addClass('itemBody').text( queue[i].description );
 
@@ -129,37 +136,38 @@ function setListBody( queue, isLoggedin ) {
             .append( body )
 
 		$('#listbody').append( item );
-
-        $('#emailButton').click(function(){
-            chrome.extension.sendMessage({directive: "panel-email-items" } );
-        })
-
-        $('#clearButton').click( function() {
-            chrome.extension.sendMessage({directive: "panel-clearButton-click"});
-        });
-
-        $('#newGroupButton').click( function() {
-            var groupName = prompt('Enter a name for group:');
-
-            if( groupName != null )
-                chrome.extension.sendMessage({directive: "addNewGroup", groupName: groupName});
-        });
-
-        $('#joinGroupButton').click( function() {
-            var groupName = prompt('Enter name of group that you want to join:');
-
-            if( groupName != null )
-                chrome.extension.sendMessage({directive: "joinToGroup", groupName: groupName});
-        });
-
 	}
-  //document.getElementById('listbody').innerHTML = listBody;
+
+    $('#emailButton').click(function(){
+        chrome.extension.sendMessage({directive: "panel-email-items" } );
+    })
+
+    $('#clearButton').click( function() {
+        chrome.extension.sendMessage({directive: "panel-clearButton-click"});
+    });
+
+    $('#groupManagerButton').click( function() {
+        $("#groupDialog").dialog("open");
+    });
 
     $(".itemCopy").click(function(){
-        var title = $(this).parent().find('.itemTitle');
-        var txt = '*' + title.text().trim() + '*  (' + title.attr('href') + ')';
+        var txt = '*' + $(this).attr('title') + '*  (' + $(this).attr('url') + ')';
 
         prompt ("Copy link, then click OK.", txt);
+    });
+
+    $("#groupDialog").dialog({autoOpen: false});
+
+    $("#groupDialog #createGroupButton").click( function() {
+        createNewGroup( $("#groupDialog #newGroupName").val() );
+    });
+
+    $("#groupDialog #joinGroupButton").click( function() {
+        joinGroup( $("#groupDialog #joinGroupName").val() );
+    });
+
+    $("#groupDialog #inviteGroupButton").click( function() {
+        inviteGroup( $("#groupDialog #inviteGroupName").val(), $("#groupDialog #inviteEmail").val() );
     });
 
     console.log( chromeExOAuth.hasToken() );
@@ -170,3 +178,18 @@ document.addEventListener('DOMContentLoaded', function () {
    	chrome.extension.sendMessage({directive: "panel-open"}, function(response) {});
 
 })
+
+function createNewGroup(groupName) {
+    if( groupName != null )
+        chrome.extension.sendMessage({directive: "addNewGroup", groupName: groupName});
+}
+
+function joinGroup(groupName) {
+    if( groupName != null )
+        chrome.extension.sendMessage({directive: "joinToGroup", groupName: groupName});
+}
+
+function inviteGroup(groupName,email) {
+    if( email != null && groupName != null )
+        chrome.extension.sendMessage({directive: "inviteToGroup", groupName: groupName, email: email});
+}
